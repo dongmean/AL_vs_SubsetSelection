7b4ed5682a16bb2bdcfa2e405756c604e6d504b0
dongmean <pdm102207@gmail.com>
2023-02-15 16:12:52 +0900

    Fixed a bug on train_transformation

--- DeepCore/deepcore/datasets/cifar100.py
+++ DeepCore/deepcore/datasets/cifar100.py
@@ -1,15 +1,18 @@
 from torchvision import datasets, transforms
+from torchvision import transforms as T
 from torch import tensor, long
 
-def CIFAR100(data_path):
+def CIFAR100(args):
     channel = 3
     im_size = (32, 32)
     num_classes = 100
     mean = [0.5071, 0.4865, 0.4409]
     std = [0.2673, 0.2564, 0.2762]
-    transform = transforms.Compose([transforms.ToTensor(), transforms.Normalize(mean=mean, std=std)])
-    dst_train = datasets.CIFAR100(data_path, train=True, download=True, transform=transform)
-    dst_test = datasets.CIFAR100(data_path, train=False, download=True, transform=transform)
+    train_transform = T.Compose(
+        [T.RandomHorizontalFlip(), T.RandomCrop(size=32, padding=4), T.ToTensor(), T.Normalize(mean=mean, std=std)])
+    test_transform = T.Compose([T.ToTensor(), T.Normalize(mean=mean, std=std)])
+    dst_train = datasets.CIFAR100(args.data_path + '/cifar100', train=True, download=False, transform=train_transform)
+    dst_test = datasets.CIFAR100(args.data_path+ '/cifar100', train=False, download=False, transform=test_transform)
     class_names = dst_train.classes
     dst_train.targets = tensor(dst_train.targets, dtype=long)
     dst_test.targets = tensor(dst_test.targets, dtype=long)
